ebb8048e67279b10d81be187a279148c1a770ea4
Philippe <philippe@iMac-de-Philippe.local>
2026-01-27 14:12:50 +0100

    Initialisation du projet Loto Sonore V2

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+?><!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Loto Sonore V2</title>
+</head>
+<body>
+    <h1>Loto Sonore V2</h1>
+    <p>Environnement initialisé correctement.</p>
+</body>
+</html>
